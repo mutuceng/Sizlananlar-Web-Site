410c0da538abfb649d24ab4ce9e39317f9a2a5ae
mutu <umuttanriverdiceng@gmail.com>
2023-12-24 12:38:20 +0300

    profil butonları calısıo

--- Ana-Sayfa/Sizlaniyor.js
+++ Ana-Sayfa/Sizlaniyor.js
@@ -5,11 +5,15 @@ $(document).ready(function ()
         function increaseCounter() 
         {
             counterValue++;
-            $('.sayac >h2').text("Çözüme Ulaşan Sızlanma Sayısı: " + counterValue);
+            $('.sayac > h2').text("Çözüme Ulaşan Sızlanma Sayısı: " + counterValue);
         }
 
         setInterval(increaseCounter, 5000);
 
+        
+        $('.uyeol-button').click(function(){ window.location.href = 'giris_yap.html'; });
+        $('.profile-button').click(function(){ window.location.href = '../profil-bilgisi/profil.html'; });
+        
         $('#sizlanForm').submit(function (event) 
         {
             event.preventDefault();
@@ -19,39 +23,25 @@ $(document).ready(function ()
             var sizlan = $('#sizlan').val();
             var secilenFirma = $('#firma').val();
             var tarih = $('#tarih').val();
+            var sikayetdurumu = 'Cevap Bekliyor'
 
             var yeniCarouselItem =
                 '<div class="carousel-item">' +
                 '<h1 class="sistem_kutu" style="color: #102317;">' + baslik + '</h1>' +
                 '<div class="sitem_kutu">' + '<div class="kime">Kime: ' + secilenFirma + '</div>' + '<p>' + sizlan + '</p>' +
                 '<div class="tarih">Tarih: ' + tarih + '</div>' + '</div>' + '<h2 class="sistem_kutu"><b>Sitemkar: </b>' + kullanici_adi + '</h2>' + 
-                '<h2 class="sistem_kutu"><b>Durum:</b><i style="color:crimson;">Cevap Bekliyor</i></h2>' + '</div>';
+                '<h2 class="sistem_kutu"><b>Durum:</b><i style="color:crimson;"> '+ sikayetdurumu + '</i></h2>' + '</div>';
+
+            var yeniCardItem = '';
 
             $('.carousel-inner').append(yeniCarouselItem);
+            $('#profilCarousel').append(yeniCarouselItem);
 
             $('#sizlanForm')[0].reset();
         })
+        
+
+        
+
+
     });
-/* $(document).ready(function () {
-    $('#sizlanForm').submit(function (event) {
-        event.preventDefault();
-
-        var baslik = $('#baslik').val();
-        var kullanici_adi = $('#kullaniciadi').val();
-        var sizlan = $('#sizlan').val();
-        var secilenFirma = $('#firma').val();
-        var tarih = $('#tarih').val();
-
-        var yeniCarouselItem = 
-        '<div class="carousel-item">' +
-        '<h1 class="sistem_kutu" style="color: #102317;">' + baslik + '</h1>' +
-        '<div class="sitem_kutu">' + '<div class="kime">' + secilenFirma + '</div>' + '<p>' sizlan + '</p>'+ 
-        '<div class="tarih">' + tarih + '</div' + '</div>';
-
-
-        $('.carousel-inner').append(yeniCarouselItem);
-
-
-        $('#sizlanForm')[0].reset();
-    });
-}); */
